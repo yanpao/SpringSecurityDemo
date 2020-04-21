ca000a4fa1bd73fb48c629b5e0507ace9dcdd274
yanpao <lihanzhang@outlook.com>
2020-04-22 00:25:13 +0800

    修改ReadME

--- src/main/java/com/wismap/springsecuritydemo/controller/UserController.java
+++ src/main/java/com/wismap/springsecuritydemo/controller/UserController.java
@@ -10,7 +10,6 @@ import com.wismap.springsecuritydemo.utils.ResultCodeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
